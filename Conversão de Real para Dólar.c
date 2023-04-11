f0d9737b5a54136e18d035d4e6a95169714adc48
AnaB351 <116103574+AnaB351@users.noreply.github.com>
2023-04-11 14:55:47 -0300

    Update Conversão de Real para Dólar.c

--- Conversão de Real para Dólar.c
+++ Conversão de Real para Dólar.c
@@ -1,4 +1,4 @@
-/* Conversão de Real(R$) para Dólar($) */ 
+* Conversão de Real(R$) para Dólar($) */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,15 +6,16 @@
 int main (void)
 
 {
-    int numero;
-    int multiplicacao;
-
-    printf ("Digite um número");
-    scanf("%i", &numero);
+    float numero;
+float resp_multiplicacao;
     
-     multiplicacao = numero * 5.07;
-  printf("\nAproximadamente = $%i ", multiplicacao);
+    printf ("Digite um número");
+    scanf("%f", &numero);
+     resp_multiplicacao = numero * 5.07;
+  printf("\n $%.2f", resp_multiplicacao);
     
     return 0;
 }
+
+
 
